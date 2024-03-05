d0759d308e30c44d843c8f59fd8b48fed9351798
jhliu17 <junhaoliu17@gmail.com>
2024-03-05 10:37:38 -0800

    add wandb utility

---
+++ nntool/wandb.py
@@ -0,0 +1,58 @@
+import os
+import git
+import wandb
+
+from dataclasses import dataclass, field
+
+
+@dataclass
+class WandbConfig:
+    # project name in wandb
+    project: str = "YOUR_PROJECT_NAME"
+
+    # wandb user name
+    entity: str = "YOUR_ENTITY_NAME"
+
+    # wandb run name (leave blacnk to use default name)
+    name: str = ""
+
+    # wandb run notes
+    notes: str = ""
+
+    # log git hash
+    log_git_hash: bool = True
+
+    # log code
+    log_code: bool = True
+
+    # code root
+    code_root: str = "."
+
+    # code file extensions
+    code_ext: list[str] = field(default_factory=lambda: [".py", ".sh"])
+
+
+def init_wandb(args: WandbConfig, run_config: dict):
+    if "WANDB_API_KEY" in os.environ["WANDB_API_KEY"]:
+        wandb.login(key=os.environ["WANDB_API_KEY"])
+
+    if args.log_git_hash:
+        repo = git.Repo(search_parent_directories=True)
+        git_hash = repo.head.object.hexsha
+        args.notes = (
+            f"{args.notes + ', ' if args.notes else ''}" + f"git hash: {git_hash}"
+        )
+
+    wandb.init(
+        project=args.project,
+        entity=args.entity,
+        name=args.name if args.name else None,
+        notes=args.notes,
+        config=run_config,
+    )
+    wandb.run.log_code(
+        root=args.code_root,
+        include_fn=lambda path, root: any(
+            [path.endswith(ext) for ext in args.code_ext]
+        ),
+    )
