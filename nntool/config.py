c51fc633310212972dc1eff5804b912e612a8b15
jhliu17 <junhaoliu17@gmail.com>
2024-04-13 19:06:30 -0700

    add configuration tools

---
+++ nntool/config.py
@@ -0,0 +1,36 @@
+import os
+import datetime
+import warnings
+
+from functools import cache
+
+
+def get_current_time():
+    # Get the current time
+    current_time = datetime.datetime.now()
+
+    # Format the time (MDY_HMS)
+    formatted_time = current_time.strftime("%m%d%Y_%H%M%S")
+
+    return formatted_time
+
+
+@cache
+def get_output_path(append_date: bool = True) -> str:
+    """Get output path based on environment variable OUTPUT_PATH
+
+    :param append_date: append a children folder with the date time, defaults to True
+    :return: output path
+    """
+    output_path = "./"
+    if "OUTPUT_PATH" in os.environ:
+        output_path = os.environ["OUTPUT_PATH"]
+    else:
+        warnings.warn(
+            f"OUTPUT_PATH not found in environment variables. Using default path: {output_path}"
+        )
+
+    if append_date:
+        output_path = os.path.join(output_path, get_current_time())
+
+    return output_path
