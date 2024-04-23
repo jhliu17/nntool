590f4f4515e1e5a941c47cfa15e1be73164d00d3
jhliu17 <junhaoliu17@gmail.com>
2024-04-23 16:09:49 -0700

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nntool",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=[
