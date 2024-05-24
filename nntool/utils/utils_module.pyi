c8ea0ca8fe4d941916dd19cc11ce93d148e8d71a
jhliu17 <junhaoliu17@gmail.com>
2024-05-23 22:36:25 -0700

    update

--- nntool/utils/utils_module.pyi
+++ nntool/utils/utils_module.pyi
@@ -1,5 +1,5 @@
 def get_current_time() -> str: ...
-def get_output_path(append_date: bool = ...) -> str:
+def get_output_path(output_path: str = ..., append_date: bool = ...) -> str:
     """Get output path based on environment variable OUTPUT_PATH
 
     :param append_date: append a children folder with the date time, defaults to True
