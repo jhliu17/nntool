48ffcae5193cebd93cf5920ba1e587af0b320fc7
jhliu17 <junhaoliu17@gmail.com>
2024-06-12 19:00:59 -0700

    bump version to v1.0.0~!

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class CleanCython(Command):
 
 package_info = dict(
     name="nntool",
-    version="0.8.0",
+    version="1.0.0",
     python_requires=">=3.9",
     install_requires=[
         # List your package dependencies here
