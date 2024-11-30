3d9d2b98184cfde489b1d534e05d5e1f6cadfce1
jhliu17 <junhaoliu17@gmail.com>
2024-11-30 13:38:00 -0800

    update config

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("/Users/junhao/Working/code/nntool"))
+sys.path.insert(0, os.path.abspath("/Users/junhao/Hub/code/nntool"))
 
 # Configuration file for the Sphinx documentation builder.
 #
