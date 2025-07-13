3e9cc8ca30f4bb2177494abb1d5260fb63f540a2
jhliu17 <junhaoliu17@gmail.com>
2025-07-12 21:30:22 -0700

    fix linting error

--- setup.py
+++ setup.py
@@ -81,9 +81,7 @@ package_info = dict(
 if os.getenv("NNTOOL_PYTHON_BUILD"):
     setup(
         packages=find_packages(exclude=["tests"]),
-        exclude_package_data={
-            "": ["*.pyi"]
-        },  # for source release, ignore all pyi files
+        exclude_package_data={"": ["*.pyi"]},  # for source release, ignore all pyi files
         **package_info,
     )
 else:
