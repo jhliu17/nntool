8f37811ef1f348f70c35ed04450b9d0455f2e3c7
jhliu17 <junhaoliu17@gmail.com>
2025-12-28 22:07:33 -0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ from typing import Union
 
 import nntool
 
-project = "🚂 NNTool"
+project = "NNTool"
 copyright = "2025"
 author = "Junhao Liu"
 repository = "https://github.com/jhliu17/nntool"
@@ -32,7 +32,10 @@ extensions = [
     # "sphinx_autodoc_typehints",
     "sphinx.ext.napoleon",
     "sphinx_copybutton",
+    "myst_parser",
+    "sphinx_design"
 ]
+myst_enable_extensions = ["colon_fence"]
 
 autodoc_default_options = {
     "members": True,
@@ -74,23 +77,32 @@ def linkcode_resolve(domain: str, info: dict) -> Union[str, None]:
         return None
 
 
-# add_function_parentheses = False
-# default_role = "literal"
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "furo"
 html_static_path = ["_static"]
-# html_css_files = ["style/custom.css"]
-# html_theme_options = {"collapse_navbar": False, "show_toc_level": 2}
 templates_path = ["_templates"]
 html_css_files = [
     "custom.css",
 ]
-html_show_sourcelink = False
-html_sourcelink_suffix = ""
+html_logo = "./_static/apple-touch-icon.png"
+html_favicon = "./_static/favicon.ico"
+html_title = "nntool"
+html_show_sourcelink = True
 html_theme_options = {
-    # "sidebar_hide_name": True,
+    "sidebar_hide_name": True,
+    "source_repository": "https://github.com/jhliu17/nntool",
+    "source_branch": "main",
+    "source_directory": "docs/",
     "top_of_page_buttons": ["view"],
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/jhliu17/nntool",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
 }
